ab4eb7b7cfe29b110c8292b39e60b1fadeb1fed7
Angela <commercial.araujo@gmail.com>
2023-10-18 14:56:37 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/br/com/angela/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/br/com/angela/blogpessoal/configuration/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package br.com.angela.blogpessoal.configuration;
 
-
-
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/br/com/angela/blogpessoal/security/UserDetailsImpl.java
+++ src/main/java/br/com/angela/blogpessoal/security/UserDetailsImpl.java
@@ -21,7 +21,7 @@ public class UserDetailsImpl implements UserDetails {
 		this.password = user.getSenha();
 	}
 	
-	//public userDetailsImpl() {}
+	public UserDetailsImpl() {}
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
